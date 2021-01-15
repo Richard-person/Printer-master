736a5ae8d50976e437458ceb751caaa4a0092697
xiejiao <1582980368@qq.com>
2021-01-15 16:06:52 +0800

    更改注释

--- module_printer/src/main/java/com/richard/printer/enumerate/EllipsizeMode.java
+++ module_printer/src/main/java/com/richard/printer/enumerate/EllipsizeMode.java
@@ -9,17 +9,17 @@ package com.richard.printer.enumerate;
 public enum EllipsizeMode {
 
     /**
-     * 文本一行若显示不完，则结尾处以省略号代替
+     * 文本在该列显示不完时，则结尾处以省略号代替
      */
     ELLIPSIS,
 
     /**
-     * 文本显示不完时，则单独行跨列显示
+     * 文本在该列显示不完时，则单独行并跨列显示
      */
     LINE,
 
     /**
-     * 当某一列显示不完时，则在该列多行显示
+     * 文本在该列显示不完时，则在该列多行显示
      */
     COLUMN_LINE;
 
